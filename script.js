91e25d4b28db1ab23e09a90ed80d9ffbbebb1cb1
Konstantin Aksenov <kav.pbg@gmail.com>
2024-02-20 21:11:11 +0100

    style probleme

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const STORAGE_TOKEN = "P3KDB0PTEEJCOX7KR2BOPMHMSBQ89ZMS58AIISJ7";
+const STORAGE_TOKEN = "";
 const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
 
 async function init() {
   await includeHTML();
-  await loadAllUsers();
 }
