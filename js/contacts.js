a89f261a0d17f3702c72c5af935731158a85aa75
clenncode <145873674+clenncode@users.noreply.github.com>
2024-02-22 15:51:03 +0100

    implement new color var and delete header / footer elements

---
+++ js/contacts.js
@@ -0,0 +1,44 @@
+ /* Mock-Up Array Kontaktdaten */
+ let contacts = [{
+    "name": "Anton Mayer", 
+    "email": "antom@gmail.com", 
+    "signature": "AM",
+    "bk-color": ""
+    }, 
+    {
+        "name": "Anja Schulz", 
+        "email": "schulz@htomail.com",
+        "signature": "AZ", 
+        "bk-color": "#FF7A00" //orange
+    },
+    {
+        "name": "Benedikt Ziegler", 
+        "email": "benedikt@gmail.com",
+        "signature": "BZ", 
+        "bk-color": "#9327FF" //violett
+    },
+    {
+        "name": "David Eisenberg", 
+        "email": "davidberg@gmail.com",
+        "signature": "DE",
+        "bk-color": "#FC71FF" //pink
+    },
+    {
+        "name": "Eva Fischer", 
+        "email": "eva@gmail.com",
+        "signature": "EF",
+        "bk-color": "#FFBB2B" //curry
+    },
+    {
+        "name": "Emmanuel Mauer", 
+        "email": "emmanuelma@gmail.com",
+        "signature": "EM",
+        "bk-color": "#1FD7C1" //green
+    }, 
+    {
+        "name": "emil Schneck", 
+        "email": "eschneck@gmail.com",
+        "signature": "ES",
+        "bk-color": "#1FD7C1" //green
+    }
+];
