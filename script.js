ab8dbc902bfa487211f8d14090c072517af58bc9
Konstantin <kav.pbg@gmail.com>
2024-02-25 23:52:33 +0100

    Kontent anpassen

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-"use strict";
-
-const STORAGE_TOKEN = "";
+const STORAGE_TOKEN = "J4LQKS1ZDS5WC5NTX6XUA3MD2ROKE6VOP8A4QBBP";
 const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
 
 async function init() {
