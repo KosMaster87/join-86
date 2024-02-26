a500ad665484feb9dc1378090d5a3f5eed991c70
Konstantin <kav.pbg@gmail.com>
2024-02-26 12:56:34 -0300

    nach call angepasst

--- js/register.js
+++ js/register.js
@@ -1,3 +1,5 @@
+let users = [];
+
 /**
  * To hide or show currentpassword in any fild.
  * @param {each input from origin HTML-element} passwordId
@@ -37,4 +39,38 @@ function resetBorderColor(containerId) {
 function registerUser() {
   // let RegisterBtn = document.getElementById("RegisterBtn");
   // meinButton.disabled = true;
+}
+
+function addNewUser() {
+  let registerInputName = document.getElementById("registerInputName");
+  let registerInputEmail = document.getElementById("registerInputEmail");
+  let registerInputPassword = document.getElementById("registerInputPassword");
+  let colorCode = "#ff3d00"; // Setzen Sie die Standardfarbe hier, falls erforderlich. BESSER EINE FUNKTION GENERAGE RENDOM
+
+  // Überprüfen Sie, ob alle erforderlichen Felder ausgefüllt sind.
+  // in der if abfrage validieren????????????????
+  if (registerInputName.value && registerInputEmail.value && password.value) {
+    // Hier erstellen Sie einen neuen Benutzer mit der createUser-Funktion
+    const newUser = createUser(
+      registerInputName.value,
+      registerInputEmail.value,
+      registerInputPassword.value,
+      colorCode
+    );
+    registerInputName.value = "";
+    registerInputEmail.value = "";
+    registerInputPassword.value = "";
+  } else {
+    console.error("Bitte füllen Sie alle erforderlichen Felder aus.");
+  }
+}
+
+// Aus dem Call !!!
+function getRandomColor(color) {
+  var letters = "123456789ABCDE";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 14)];
+  }
+  return color;
 }
