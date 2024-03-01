3f4cd9571a36b7ae4b0e01d38dd2334f14af162b
clenncode <145873674+clenncode@users.noreply.github.com>
2024-03-01 12:29:31 +0100

    delete mockUpContacts

--- js/contact/contacts.js
+++ js/contact/contacts.js
@@ -1,65 +1,29 @@
-const mockUpUserContacts = [
-    {
-        "userId": "user1",
-        "contactId": "33333333333333333",
-        "name": "Benedikt Ziegler", 
-        "email": "benedikt@gmail.com",
-        "phone": "0211 2222 1111",
-        "userColor": "var(--purple)", 
-        "signature": "BZ",   
-    },
-    {
-        "userId": "user1",
-        "contactId": "11111111111111111",
-        "name": "Anton Mayer", 
-        "email": "antom@gmail.com", 
-        "phone": "0311 3333 1111",
-        "userColor": "var(--mintGreen)",
-        "signature": "AM", 
-    }, 
-    {
-        "userId": "user1",
-        "contactId": "7777777777777777",
-        "name": "Emil Schneck", 
-        "email": "eschneck@gmail.com",
-        "phone": "0411 4444 1111",
-        "userColor": "var(--purple)",
-        "signature": "ES", 
-    },
-    {
-        "userId": "user1",
-        "contactId": "222222222222222",
-        "name": "Anja Schulz", 
-        "email": "schulz@htomail.com",
-        "phone": "0511 5555 1111",
-        "userColor": "var(--orangeIcons)",
-        "signature": "AZ",   
-    },
-    {
-        "userId": "user1",
-        "contactId": "666666666666666",
-        "name": "Emmanuel Mauer", 
-        "email": "emmanuelma@gmail.com",
-        "phone": "0611 6666 1111",
-        "userColor": "var(--greenIcon)",
-        "signature": "EM",  
-    }, 
-    {
-        "userId": "user1",
-        "contactId": "4444444444444444",
-        "name": "David Eisenberg", 
-        "email": "davidberg@gmail.com",
-        "phone": "0711 7777 1111",
-        "userColor": "var(--pink)", 
-        "signature": "DE",
-    },
-    {        
-        "userId": "user1",
-        "contactId": "55555555555555555",
-        "name": "Eva Fischer", 
-        "email": "eva@gmail.com",
-        "phone": "0811 8888 1111",
-        "userColor": "var(--curry)",
-        "signature": "EF",
-    },
-];
+
+//TODO: implement getter und setterItem
+//TODO: connection to storage
+//TODO: implement mockUpData  
+
+
+async function setItem(key, value) {
+    const url='https://remote-storage.developerakademie.org/item';
+    const payload = { key, value, token:STORAGE_TOKEN };
+    
+
+    return fetch(
+        url,
+        { method: 'POST', body: JSON.stringify(payload) })
+        .then(res => res.json());
+}
+
+
+async function getItem(key) {
+    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+
+    return fetch(url)
+        .then(res => res.json())
+        .then(res => {
+            if (res.data) {
+                return res.data.value;
+            } throw `Could not find data with key "${key}".`;
+        });
+}
