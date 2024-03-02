a2bfbfb715a3318debdb2dded3a83429324e3a3c
Konstantin <kav.pbg@gmail.com>
2024-03-02 22:07:49 +0100

    BUG fixed Board too

--- js/board.js
+++ js/board.js
@@ -14,13 +14,11 @@ async function initBoard() {
 
 async function loadCurrentUserAsObject() {
   const users = JSON.parse(await getItem("users")) || [];
-  const currentIndex = await getItem("currentIndex");
+  const currentEmail = await getItem("currentUserId");
 
-  if (currentIndex) {
+  if (currentEmail) {
     // Lade den Benutzer anhand des Index aus dem `users`-Array
-    user = users.find(
-      (userIndex) => userIndex.index === parseInt(currentIndex)
-    );
+    user = users.find((userIndex) => userIndex.email === currentEmail);
     console.log(user);
   } else {
     console.log("Benutzer nicht gefunden");
