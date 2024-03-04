b950f34fc45000d1fac1ee6312a7fbe14b2cf02c
Sascha Lenz <lenz.sascha.1994@gmail.com>
2024-03-04 17:48:55 +0100

    board edits

--- js/board.js
+++ js/board.js
@@ -10,10 +10,16 @@ async function initBoard() {
   await includeHTML();
   setActiveLink("navBoard");
   await loadCurrentUserAlsoUsersAsObject();
+  loadTasks();
 }
 
+//  for (let i = 0; i < user.tasks.length; i++) {
+function loadTasks(){
 
-
+    title = document.getElementById(`titleid`);
+    titletext= user.tasks[1].title;
+    title.innerHTML = titletext;
+}
 // --------------------------------------------------------
 
 
