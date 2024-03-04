25786c184cc53b5e0b439327d045d72548a03208
Sascha Lenz <lenz.sascha.1994@gmail.com>
2024-03-04 16:55:31 +0100

    many pushs

--- js/board.js
+++ js/board.js
@@ -12,21 +12,10 @@ async function initBoard() {
   await loadCurrentUserAlsoUsersAsObject();
 }
 
-/**
- * Provide the user globally as an object and the other users in a separate array.
- */
-async function loadCurrentUserAlsoUsersAsObject() {
-  users = JSON.parse(await getItem("users")) || [];
-  const currentEmail = await getItem("currentUserId");
 
-  if (currentEmail) {
-    // Lade den Benutzer anhand des Index aus dem `users`-Array
-    user = users.find((userIndex) => userIndex.email === currentEmail);
-    console.log(users);
-    console.log(user);
-  } else {
-    console.log("Benutzer nicht gefunden");
-  }
-}
+
+// --------------------------------------------------------
+
+
 
 // --------------------------------------------------------
--- js/summary.js
+++ js/summary.js
@@ -101,23 +101,6 @@ async function initSummary() {
   initSummeryCountings();
 }
 
-/**
- * Provide the user globally as an object and the other users in a separate array.
- */
-async function loadCurrentUserAlsoUsersAsObject() {
-  users = JSON.parse(await getItem("users")) || [];
-  const currentEmail = await getItem("currentUserId");
-
-  if (currentEmail) {
-    // Lade den Benutzer anhand des Index aus dem `users`-Array
-    user = users.find((userIndex) => userIndex.email === currentEmail);
-    console.log(users);
-    console.log(user);
-  } else {
-    console.log("Benutzer nicht gefunden");
-  }
-}
-
 // --------------------------------------------------------
 
 /**
