dbcea930fc750f03f57cba9f589bc793e272ebd9
Sascha Lenz <lenz.sascha.1994@gmail.com>
2024-03-05 10:56:22 +0100

    add task edits

--- js/addTaskHTML.js
+++ js/addTaskHTML.js
@@ -277,14 +277,14 @@ function openCategorySelectReturn() {
 
 function subtastwindowReturn() {
   return `<img src="../assets/img/add_task/task_line.svg" />
-    <img onclick="back()" src="../assets/img/add_task/task_check.svg" />`;
+    <img class="arrow" onclick="back()" src="../assets/img/add_task/task_check.svg" />`;
 }
 
 function changemenuReturn() {
   return `
-    <img src="../assets/img/add_task/task_cross.svg" onclick="clearSubtaskInputfield()"/>
+    <img class="arrow" src="../assets/img/add_task/task_cross.svg" onclick="clearSubtaskInputfield()"/>
     <img src="../assets/img/add_task/task_line.svg"/>
-    <img onclick="addSubtask()" src="../assets/img/add_task/task_check.svg"/>`;
+    <img class="arrow" onclick="addSubtask()" src="../assets/img/add_task/task_check.svg"/>`;
 }
 
 function renderSubtasksReturn(subtasks, i) {
@@ -295,9 +295,9 @@ function renderSubtasksReturn(subtasks, i) {
         <P>${subtasks[i]}</P>
       </div>
       <div class="subMenu">
-        <img src="../assets/img/add_task/task_edit.svg" onclick="editSubtask(${i})" alt="edit_icon">
+        <img class="arrow" src="../assets/img/add_task/task_edit.svg" onclick="editSubtask(${i})" alt="edit_icon">
         <img src="../assets/img/add_task/task_line.svg" alt="subtasks_seperator">
-        <img src="../assets/img/add_task/task_cross.svg" onclick="deleteSubtask(${i})" alt="delete_icon">
+        <img class="arrow" src="../assets/img/add_task/task_cross.svg" onclick="deleteSubtask(${i})" alt="delete_icon">
       </div>
     </div>
   </div>`;
@@ -307,9 +307,9 @@ function editSubtaskReturn(subtasks, i) {
   return `<div class="subtaskEdit" id="subtaskEdit">
     <input type="text" id="editSubtask${i}" value="${subtasks[i]}">
     <div class="subtastEditMenu">
-      <img src="../assets/img/add_task/task_bin.svg" onclick="deleteSubtask(${i})" alt="delete_icon">
+      <img class="arrow" src="../assets/img/add_task/task_bin.svg" onclick="deleteSubtask(${i})" alt="delete_icon">
       <img src="../assets/img/add_task/task_line.svg" alt="subtasks_seperator">
-      <img src="../assets/img/add_task/task_check.svg" onclick="editSubtaskDone(${i})" alt="done_icon">
+      <img class="arrow" src="../assets/img/add_task/task_check.svg" onclick="editSubtaskDone(${i})" alt="done_icon">
     </div>
   </div>`;
 }
