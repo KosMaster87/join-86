71f054c1c0ef33eb3674df1a3177d31ba631c37f
Sascha Lenz <lenz.sascha.1994@gmail.com>
2024-03-07 14:41:18 +0100

    board push

--- js/board.js
+++ js/board.js
@@ -28,7 +28,7 @@ function loadTasks() {
 
   for (let i = 0; i < user.tasks.length; i++) {
     if (user.tasks[i].status === "to-do") {
-      toDoMainContainer.innerHTML += toDoHtmlReturn(i);
+      toDoMainContainer.innerHTML += HtmlReturn(i);
       taskCategoryContainer = document.getElementById(`TaskCategory${i}`);
       titleContainer = document.getElementById(`titleId${i}`);
       descriptionContainer = document.getElementById(`descriptionID${i}`);
@@ -38,6 +38,7 @@ function loadTasks() {
       howManyTasks = document.getElementById(`counterOfTasks${i}`);
       finishedTasks = document.getElementById(`finishedTasks${i}`);
 
+      prioContainer.src = "../assets/img/board/board_low.svg";
       howManyTasks.innerHTML = `${user.tasks[i].subtasks.length}`;
       titleContainer.innerHTML = user.tasks[i].title;
       descriptionContainer.innerHTML = user.tasks[i].description;
@@ -61,4 +62,150 @@ function loadTasks() {
       }
     }
   }
+  loadProgressTasks();
+}
+
+function loadProgressTasks() {
+  toDoMainContainer = document.getElementById(`progressMainContainer`);
+
+  for (let i = 0; i < user.tasks.length; i++) {
+    if (user.tasks[i].status === "progress") {
+      toDoMainContainer.innerHTML += HtmlReturn(i);
+      taskCategoryContainer = document.getElementById(`TaskCategory${i}`);
+      titleContainer = document.getElementById(`titleId${i}`);
+      descriptionContainer = document.getElementById(`descriptionID${i}`);
+      progressContainer = document.getElementById(`ProgressBar${i}`);
+      iconBarContainer = document.getElementById(`IconBar${i}`);
+      prioContainer = document.getElementById(`PrioImageContainer${i}`);
+      howManyTasks = document.getElementById(`counterOfTasks${i}`);
+      finishedTasks = document.getElementById(`finishedTasks${i}`);
+
+      prioContainer.src = "../assets/img/board/board_low.svg";
+      howManyTasks.innerHTML = `${user.tasks[i].subtasks.length}`;
+      titleContainer.innerHTML = user.tasks[i].title;
+      descriptionContainer.innerHTML = user.tasks[i].description;
+      taskCategoryContainer.innerHTML = user.tasks[i].category;
+
+      if (user.tasks[i].category === "User Story") {
+        taskCategoryContainer.style.backgroundColor = "#0038FF";
+      } else if (user.tasks[i].category === "Technical Task") {
+        taskCategoryContainer.style.backgroundColor = "#1FD7C1";
+      }
+
+      for (let a = 0; a < user.tasks[i].assignedTo.length; a++) {
+        let signature = "";
+        let words = user.tasks[i].assignedTo[a].toUpperCase().split(" ");
+
+        for (let j = 0; j < words.length; j++) {
+          signature += words[j].charAt(0);
+        }
+
+        iconBarContainer.innerHTML += iconReturn(signature);
+      }
+    }
+  }
+  loadAwaitTasks();
+}
+
+function loadAwaitTasks() {
+  toDoMainContainer = document.getElementById(`awaitMainContainer`);
+
+  for (let i = 0; i < user.tasks.length; i++) {
+    if (user.tasks[i].status === "await") {
+      toDoMainContainer.innerHTML += awaitHtmlReturn(i);
+      taskCategoryContainer = document.getElementById(`TaskCategory${i}`);
+      titleContainer = document.getElementById(`titleId${i}`);
+      descriptionContainer = document.getElementById(`descriptionID${i}`);
+      progressContainer = document.getElementById(`ProgressBar${i}`);
+      iconBarContainer = document.getElementById(`IconBar${i}`);
+      prioContainer = document.getElementById(`PrioImageContainer${i}`);
+      finishedTasks = document.getElementById(`finishedTasks${i}`);
+
+      prioContainer.src = "../assets/img/board/board_low.svg";
+      titleContainer.innerHTML = user.tasks[i].title;
+      descriptionContainer.innerHTML = user.tasks[i].description;
+      taskCategoryContainer.innerHTML = user.tasks[i].category;
+
+      if (user.tasks[i].category === "User Story") {
+        taskCategoryContainer.style.backgroundColor = "#0038FF";
+      } else if (user.tasks[i].category === "Technical Task") {
+        taskCategoryContainer.style.backgroundColor = "#1FD7C1";
+      }
+
+      for (let a = 0; a < user.tasks[i].assignedTo.length; a++) {
+        let signature = "";
+        let words = user.tasks[i].assignedTo[a].toUpperCase().split(" ");
+
+        for (let j = 0; j < words.length; j++) {
+          signature += words[j].charAt(0);
+        }
+
+        iconBarContainer.innerHTML += iconReturn(signature);
+      }
+    }
+  }
+  loadDoneTasks();
+}
+
+function loadDoneTasks() {
+  toDoMainContainer = document.getElementById(`doneMainContainer`);
+
+  for (let i = 0; i < user.tasks.length; i++) {
+    if (user.tasks[i].status === "done") {
+      toDoMainContainer.innerHTML += HtmlReturn(i);
+      taskCategoryContainer = document.getElementById(`TaskCategory${i}`);
+      titleContainer = document.getElementById(`titleId${i}`);
+      descriptionContainer = document.getElementById(`descriptionID${i}`);
+      progressContainer = document.getElementById(`ProgressBar${i}`);
+      iconBarContainer = document.getElementById(`IconBar${i}`);
+      prioContainer = document.getElementById(`PrioImageContainer${i}`);
+      howManyTasks = document.getElementById(`counterOfTasks${i}`);
+      finishedTasks = document.getElementById(`finishedTasks${i}`);
+
+      prioContainer.src = "../assets/img/board/board_low.svg";
+      howManyTasks.innerHTML = `${user.tasks[i].subtasks.length}`;
+      titleContainer.innerHTML = user.tasks[i].title;
+      descriptionContainer.innerHTML = user.tasks[i].description;
+      taskCategoryContainer.innerHTML = user.tasks[i].category;
+
+      if (user.tasks[i].category === "User Story") {
+        taskCategoryContainer.style.backgroundColor = "#0038FF";
+      } else if (user.tasks[i].category === "Technical Task") {
+        taskCategoryContainer.style.backgroundColor = "#1FD7C1";
+      }
+
+      for (let a = 0; a < user.tasks[i].assignedTo.length; a++) {
+        let signature = "";
+        let words = user.tasks[i].assignedTo[a].toUpperCase().split(" ");
+
+        for (let j = 0; j < words.length; j++) {
+          signature += words[j].charAt(0);
+        }
+
+        iconBarContainer.innerHTML += iconReturn(signature);
+      }
+    }
+  }
+  noInner();
+}
+
+
+function noInner(){
+  let TodoMainContainer = document.getElementById('TodoMainContainer');
+  let progressMainContainer = document.getElementById('progressMainContainer');
+  let awaitMainContainer = document.getElementById('awaitMainContainer');
+  let doneMainContainer = document.getElementById('doneMainContainer');
+
+if (TodoMainContainer.innerHTML.trim() === '') {
+  TodoMainContainer.innerHTML = '<div class="noTasksInThisSelection font16400">No tasks To do</div>';
+}
+if (progressMainContainer.innerHTML.trim() === '') {
+  progressMainContainer.innerHTML = '<div class="noTasksInThisSelection font16400">No tasks in progress</div>';
+}
+if (awaitMainContainer.innerHTML.trim() === '') {
+  awaitMainContainer.innerHTML = '<div class="noTasksInThisSelection font16400">No tasks await</div>';
+}
+if (doneMainContainer.innerHTML.trim() === '') {
+  doneMainContainer.innerHTML = '<div class="noTasksInThisSelection font16400">No tasks done</div>';
+}
 }
--- js/boardHTML.js
+++ js/boardHTML.js
@@ -1,14 +1,18 @@
-function toDoHtmlReturn(i){
-    return `
-    <div id="toDoContainer${i}" class="boxSizeProgress">
+function HtmlReturn(i) {
+  return`
+  <div id="toDoContainer${i}" class="boxSizeProgress">
     <div class="headContainer">
-      <span id="TaskCategory${i}" class="headlineFromTask font16400">Catergory</span>
+      <span id="TaskCategory${i}" class="headlineFromTask font16400">
+        Catergory
+      </span>
       <div>
         <img src="../assets/img/board/mobile_switch.svg" />
       </div>
     </div>
     <div class="inProgressText">
-      <p class="titleText font16700" id="titleId${i}">Kochwelt Page & Recipe Recommender</p>
+      <p class="titleText font16700" id="titleId${i}">
+        Kochwelt Page & Recipe Recommender
+      </p>
       <p class="descriptionText font16400" id="descriptionID${i}">
         Build start page with recipe recommendation...
       </p>
@@ -18,24 +22,46 @@ function toDoHtmlReturn(i){
         <div class="progress-bar" id="ProgressBar"></div>
       </div>
       <p class="font12400">
-        <p id="finishedTasks${i}">0</p>
-        /
-        <p id="counterOfTasks${i}">2</p>
+        <p id="finishedTasks${i}">0</p>/<p id="counterOfTasks${i}">2</p>
         Subtasks
       </p>
     </div>
     <div class="progressFooter">
-      <div class="IconBarClass" id="IconBar${i}">
+      <div class="IconBarClass" id="IconBar${i}"></div>
+      <div>
+        <img id="PrioImageContainer${i}" />
       </div>
-      <div><img id="PrioImageContainer${i}" src="../assets/img/board/board_low.svg" /></div>
     </div>
-  </div>
-    `
+  </div>;`
+}
+function awaitHtmlReturn(i) {
+  return`
+  <div id="waitContainer${i}" class="boxSizeFeedback">
+    <div class="headContainer">
+      <span id="TaskCategory${i}">Technical Task</span>
+      <div>
+        <img src="../assets/img/board/mobile_switch.svg" />
+      </div>
+    </div>
+    <div class="inProgressText">
+      <p class="titleText font16700" id="titleId${i}">
+        Kochwelt Page & Recipe Recommender
+      </p>
+      <p class="descriptionText font16400" id="descriptionID${i}">
+        Build start page with recipe recommendation...
+      </p>
+    </div>
+    <div class="progressFooter">
+      <div class="IconBarClass" id="IconBar${i}"></div>
+      <div>
+        <img id="PrioImageContainer${i}" />
+      </div>
+    </div>
+  </div>;`
 }
 
-
-function iconReturn(signature){
-    return`
+function iconReturn(signature) {
+  return `
     <div class="iconstlye" style="background-color: red; margin-left: -10px">${signature}</div>
-    `
+    `;
 }
